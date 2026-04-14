b4d068841cb940718401d208543d84ab844bf53e
David Studholme <d.j.studholme@exeter.ac.uk>
2026-04-14 18:49:06 +0000

    first commit

---
+++ 02_ref/commands.sh
@@ -0,0 +1,2 @@
+ln -s ../01_genomes/V._cholerae_FDAARGOS_103.fasta  .
+
---
+++ 03_workdir/commands.sh
@@ -0,0 +1,7 @@
+ln -s ../01_genomes/*.contig .
+ln -s ../01_genomes/*final.fasta .
+ln -s ../01_genomes/*unicycler.fasta .
+
+rm V._cholerae_FDAARGOS_103.contig 
+
+
