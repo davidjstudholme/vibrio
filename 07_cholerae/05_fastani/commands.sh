69f8bb96aa3ddb1029a9c7fd5c341281d1dc99f4
David Studholme <d.j.studholme@exeter.ac.uk>
2026-04-20 20:25:55 +0000

    Ran fastANI

--- 07_cholerae/05_fastani/commands.sh
+++ 07_cholerae/05_fastani/commands.sh
@@ -2,9 +2,14 @@
 ls ../01_genomes/GCA_*.fna > ref_list.txt 
 ls ../01_genomes/11813_EXE_05_2024_A_PBC74148_final.fasta > query_list.txt
 
+split -l 1000 -d ref_list.txt chunk_
+
 ### FastANI has already been installed via Conda
 conda activate fastani_env
 conda list -n fastani_env > fastani_env_packages.txt
 conda env export > fastani_env.yaml
 
-fastANI --ql query_list.txt --rl ref_list.txt -o cholerae-fastANI.short.out -t 6 --visualize --matrix
+for i in chunk_*; do
+    echo $i
+    fastANI --ql query_list.txt --rl $i -o cholerae-fastANI.$i.short.out -t 6 --visualize --matrix
+done
