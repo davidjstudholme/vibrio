de16c0039337405d6c89a18cbb53c19bc46ff263
David Studholme <d.j.studholme@exeter.ac.uk>
2026-05-20 16:40:13 +0000

    Added Hybracter stuff

---
+++ 01_PhaME/03_workdir_jasicida/commands.sh
@@ -0,0 +1,4 @@
+ln -s ../01_genomes/*.fasta .
+ln -s ../01_genomes/exeter_genomes/* .
+rm Vibrio_jasicida_CAIM_1864.fasta
+
---
+++ 01_PhaME/04_workdir_cholerae/commands.sh
@@ -0,0 +1,4 @@
+ln -s ../01_genomes/*.fasta .
+ln -s ../01_genomes/exeter_genomes/* .
+rm Vibrio_cholerae_FDAARGOS_103.fasta
+
